761a9d73ce746721b6a56ac2159009c426ffd7ab
Jay <jayvaidya3009@gmail.com>
2025-10-26 10:51:27 +0530

    Add skeleton routes for user

--- index.js
+++ index.js
@@ -6,19 +6,28 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/signup", function(req, res){
+app.post("/user/signup", function (req, res) {
+    res.json({
+        message: "Signup endpoint"
+    })
+})
+
+app.post("/user/signin", function (req, res) {
+    res.json({
+        message: "Signin Endpoint"
+    })
+})
+
+app.get("/user/purchases", function (req, res) {
 
 })
 
-app.post("/signin", function(req, res){
+app.post("/course/purchase", function(req, res){
 
 })
 
-app.post("/purchaseCourse", function(req,res){
 
-})
-
-app.get("/courses", function(req, res){
+app.get("/courses", function (req, res) {
 
 })
 
