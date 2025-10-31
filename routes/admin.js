aa3333fcb7c6ae4a660b1a2dc03c31b1196dcb03
Jay <jayvaidya3009@gmail.com>
2025-10-31 18:55:36 +0530

    Setup signin and signup endpoint

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,50 @@
 import Router from "express";
-const adminRouter = Router();
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
 import { AdminModel } from "../db.js";
+const adminRouter = Router();
+const JWT_ADMIN_SECRET = "SECRET@ADMIN";
+//TODO
+//Implement cookie based authentication
+//Add Zod validation
+//Try catch block in the necessary code
+adminRouter.post("/signup", async function (req, res) {
+ const email = req.body.email;
+ const password = req.body.password;
 
-adminRouter.post("/signup", function (req, res) {});
+ const hashedpassword = bcrypt.hash(password, 5);
+ 
+ await AdminModel.create({ 
+    email,
+    password: hashedpassword 
+ })
 
-adminRouter.post("/signin", function (req, res) {});
+ res.json({
+    message: "You are signed up!"
+ })
+
+});
+
+adminRouter.post("/signin", async function (req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const response = await AdminModel.findOne({
+        email
+    })
+
+    if(!response){
+        res.json({
+            message: "Please sigin up!"
+        })
+    }
+    
+    const hashedpassword = bcrypt.compare(password, response.password);
+
+    
+
+    
+});
 
 adminRouter.get("/", function (req, res) {});
 
--- routes/user.js
+++ routes/user.js
@@ -1,42 +1,92 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import { UserModel } from "../db.js";
 const userRouter = express.Router();
+const JWT_USER_SECRET = "SECRET@USER";
+
+//Auth middleware
+async function auth(req, res, next) {
+  const token = req.headers.token;
+  const decodedToken = jwt.verify(token, JWT_SECRET);
+  
+  console.log(decodedToken);
+  if (token === decodedToken) {
+    next();
+  } else {
+    res.json({
+      message: "Please sign in!"
+    })
+  }
+}
 
 userRouter.post("/signup", async function (req, res) {
   const email = req.body.email;
   const password = req.body.password;
 
   const duplicateEmail = await UserModel.findOne({
-    email
-  })
+    email,
+  });
 
-  if(duplicateEmail){
+  if (duplicateEmail) {
     return res.json({
-      message: "Email already exists!"
-    })
+      message: "Email already exists!",
+    });
   }
 
-  const hashedpassword = await bcrypt.hash(password, 5, async function(err, hash){
-     await UserModel.create({
-      email,
-      password: hash
-    })
-  })
+  const hashedpassword = await bcrypt.hash(
+    password,
+    5,
+    async function (err, hash) {
+      await UserModel.create({
+        email,
+        password: hash,
+      });
+    }
+  );
+
 
   res.json({
-    message: "You are signed up!"
-  })
-
-  
-});
-
-userRouter.post("/signin", function (req, res) {
-  res.json({
-    message: "Signin Endpoint",
+    message: "You are signed up!",
   });
 });
 
-userRouter.get("/purchases", function (req, res) {});
+userRouter.post("/signin", async function (req, res) {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const response = await UserModel.findOne({
+    email,
+  });
+
+  if (!response) {
+    message: "User does not exists, Please signup!";
+  }
+
+  const hashedpassword = await bcrypt.compare(password, response.password);
+
+  if (hashedpassword) {
+    const token = jwt.sign(
+      {
+        id: response._id.toString(),
+      },
+      JWT_SECRET
+    );
+    console.log(token);
+    res.json({
+      token,
+    });
+  } else {
+    res.json({
+      message: "Invalid Credentials!",
+    });
+  }
+});
+
+userRouter.get("/purchases", auth, function (req, res) {
+  res.json({
+    message: "purchases!!",
+  });
+});
 
 export { userRouter };
